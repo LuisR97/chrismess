f0e691ddf54aa5d183a32f0ea1e0c7660afe226b
Luis Raygadas-Lara <lraygada@pnw.edu>
2018-06-26 14:56:40 -0400

    Make the form work

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+//Luis Raygadas
+
 console.log("It works!")
+
+const button = document.querySelector('button')
+function changeText()
+{
+    const header = document.querySelector('.chris')
+    header.textContent = 'So today we will be discussing more people named Chris'
+}
+
+const button2 = document.querySelector('#button2')
+function changeH()
+{
+    const header2 = document.querySelector('#m1')
+    header2.textContent = 'WE WANT CHRIS'
+}
+
+button.addEventListener('click', changeText)
+button2.addEventListener('click', changeH)
+
+const button3 = document.querySelector("#scanIn")
+function inputText()
+{
+const headerchange = document.querySelector(".chris" )
+headerchange.textContent = document.querySelector("#scanOut").value
+event.preventDefault()
+}
+button3.addEventListener('click', inputText)
